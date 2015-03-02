d8b700b0eca5142ad2dab6f2feb378003094a276
Mathias Winde <Mapede11@student.aau.dk>
2015-03-02 08:54:11 +0100

    Benytter nu den opdatered version af module_lib

--- app/src/main/java/dk/aau/cs/psylog/psylog_gyroscopemodule/GyroscopeService.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_gyroscopemodule/GyroscopeService.java
@@ -1,14 +1,10 @@
 package dk.aau.cs.psylog.psylog_gyroscopemodule;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
 import dk.aau.cs.psylog.module_lib.SuperService;
 
 public class GyroscopeService extends SuperService {
     @Override
-    public void onCreate() {
-        super.onCreate();
+    public void setSensor() {
         sensor = new GyroscopeListener(this);
     }
 }
